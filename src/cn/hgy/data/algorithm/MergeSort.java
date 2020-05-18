800c13334ed7c72c2ed8395ce4645137cde0c268
guoyu.huang <17759217302@qq.com>
2020-05-18 08:21:02 +0800

    添加归并排序

--- src/cn/hgy/data/algorithm/MergeSort.java
+++ src/cn/hgy/data/algorithm/MergeSort.java
@@ -8,4 +8,12 @@ package cn.hgy.data.algorithm;
  */
 public class MergeSort {
 
+    public static void main(String[] args) {
+        int[] arrays = {11, 8, 3, 9, 7, 1, 2, 5};
+    }
+
+    public static int[] sort() {
+        return null;
+    }
+
 }
